dc441de2f226dd0768eff1cad9b858faf0de1dfb
sercan-basaran <83455691+sercan-basaran@users.noreply.github.com>
2021-05-05 14:05:57 +0300

    Add files via upload

---
+++ KafeProjesi ödev 4/src/Main.java
@@ -0,0 +1,27 @@
+import java.time.LocalDate;
+
+import Abstract.BaseCustomerManager;
+import Adapters.MernisServiceAdapter;
+import Concrete.NeroCustomerManager;
+import Concrete.StarbucksCustýmerManager;
+import Entities.Customer;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		 BaseCustomerManager baseCustomerManager= new NeroCustomerManager();
+		 BaseCustomerManager base1CustomerManager= new StarbucksCustýmerManager(new MernisServiceAdapter());
+		 
+		 Customer customer = new Customer();
+		 customer.setId(1);
+		 customer.setFirstName("Sercan");
+		 customer.setLastName("Baþaran");
+		 customer.setDateOfBirth(LocalDate.of(1988,10,04));
+		 customer.setNationalityId("646465484965);
+		 
+		 baseCustomerManager.save(customer);
+
+	}
+
+}
+
