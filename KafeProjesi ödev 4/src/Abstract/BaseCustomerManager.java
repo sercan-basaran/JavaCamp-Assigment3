0d0c7ecd043acd4510340b9bf0126a53580cb8dc
sercan-basaran <83455691+sercan-basaran@users.noreply.github.com>
2021-05-05 14:01:23 +0300

    Delete KafeProjesi ödev 4/src directory

--- KafeProjesi ödev 4/src/Abstract/BaseCustomerManager.java
+++
@@ -1,9 +0,0 @@
-package Abstract;
-
-import Entities.Customer;
-
-public abstract class BaseCustomerManager implements CustomerService {
-
-	public abstract void save(Customer customer);
-
-}
--- KafeProjesi ödev 4/src/Abstract/CustomerCheckService.java
+++
@@ -1,9 +0,0 @@
-package Abstract;
-
-import Entities.Customer;
-
-public interface CustomerCheckService {
-	
-	boolean checkIfRealCustomer(Customer customer);
-
-}
